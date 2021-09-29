fd177374369578316181986d02697d2bdeb47e0b
30201220 <weli30201220@gmail.com>
2021-09-29 22:51:05 +0800

    202109291051

--- v1-MiniD1/v1-MiniD1.ino
+++ v1-MiniD1/v1-MiniD1.ino
@@ -1,61 +1,172 @@
 #include <SoftwareSerial.h>  
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266WiFiMulti.h>
 #include <LittleFS.h>
 #include <ArduinoJson.h>
 
-SoftwareSerial zigbeeSerial(D3, D2); // RX | TX  
+#define LED_PIN D5
+#define BUTTON_PIN D6
+
+SoftwareSerial zigbeeSerial(D3, D2); // TX | RX  
+ESP8266WiFiMulti wifiMulti;
 ESP8266WebServer httpServer(80);
 
-IPAddress IP(192,168,62,1);
-IPAddress GATEWAY(192,168,62,1);
+IPAddress IP(192,168,4,1);
+IPAddress GATEWAY(192,168,4,1);
 IPAddress SUBNET(255,255,255,0);
 
-String AP_SSID="Bus";
-String AP_PASSWORD="Bus";
-int AP_CHANNEL=8;
+char* AP_SSID = "Bus";
+char* AP_PASSWORD = "Bus";
+int AP_CHANNEL = 2;
+int buttonState = 0;
+int LEDState = 0;
 
-char* str="";
+String BusNumber;
+
+const byte numChars = 1000;
+char receivedChars[numChars];
+boolean newData = false;
+
+boolean LEDOUTPUT = false;
+
+StaticJsonDocument<200> docIpt;
 
 void setup(){
-  // Serial
+  pinMode(LED_PIN,OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  
   Serial.begin(74880);
-  Serial.println("");
-  Serial.println("Serial:");
-  Serial.println("Serial Started...");
+  //read configuration from FS json
+  Serial.println("mounting FS...");
   // zigbeeSerial
   zigbeeSerial.begin(38400);
   Serial.println("zigbeeSerial Started...");
-
   // Network
   Serial.println("Network:");
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP);
   // softAP
   WiFi.softAPConfig(IP, GATEWAY, SUBNET);        // declared as: bool softAPConfig (IPAddress local_ip, IPAddress gateway, IPAddress subnet)
   WiFi.softAP(AP_SSID, AP_PASSWORD, AP_CHANNEL);  // network name, network password, wifi channel
   Serial.println("softAP Started...");
   Serial.print("softAP IP address: ");
   Serial.println(WiFi.softAPIP());
-
   // Filesystem
   Serial.println("Filesystem:");
-  if(LittleFS.begin()){
+  /*if(LittleFS.begin()){
     Serial.println("LittleFS Started...");
   } else {
     Serial.println("LittleFS Failed to Start.");
-  }
-
+  }*/
   // HttpServer
-  //httpServer.on("/", handleRoot);
+  httpServer.on("/LED-Control", handleRoot);
   httpServer.onNotFound(handleUserRequest);
   httpServer.begin();
   Serial.println("HttpServer Started...");
   
-}  
-void loop(){  
-  httpServer.handleClient();
+  //data
+  if (LittleFS.begin())
+  {
+    Serial.println("mounted file system");
+    if (LittleFS.exists("/web/test.txt"))
+    {
+      Serial.println("reading config file");
+      // 開啟文件
+      File configFile = LittleFS.open("/web/test.txt", "r");
+      if (configFile)
+      {
+        Serial.println("opened config file");
+        // 文件內容大小
+        int s = configFile.size();
+        Serial.printf("Size=%d\r\n", s);
+
+        // 讀取文件內容
+        BusNumber = configFile.readString();
+        Serial.println(BusNumber);
+
+        configFile.close();
+      }
+      else{
+        Serial.println("opened config file failed");
+      }
+    }
+  }
+  else
+  {
+    Serial.println("failed to mount FS");
+  }
+  //end read
   
-}  
+}
+
+void loop(){
+  buttonState = digitalRead(BUTTON_PIN);
+  httpServer.handleClient();
+  recvWithStartEndMarkers();
+  showNewData();
+  digitalWrite(LED_PIN, LEDOUTPUT ? HIGH : LOW);
+  
+ 
+  
+  
+  /*String json = zigbeeSerial.readString();
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, json);
+  serializeJson(doc, Serial);
+ // const char* world = doc["KHH28_0"];
+  Serial.println(json);
+  //Serial.println(doc["KHH28_0"]);*/
+}
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '{';
+  char endMarker = '}';
+  char rc;
+
+  while (zigbeeSerial.available() > 0 && newData == false) {
+    rc = zigbeeSerial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '}';
+        ndx++;
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+      receivedChars[ndx] = '{';
+      ndx++;
+    }
+  }
+}
+
+void showNewData() {
+  if (newData == true) {
+    Serial.println(receivedChars);
+    newData = false;
+    deserializeJson(docIpt, receivedChars);
+    //serializeJsonPretty(docIpt, Serial);
+    //const char* world = docIpt["KHH28_0"];
+    //JsonObject object = docIpt.as<JsonObject>();
+    //const char* world = object["KHH28_0"];
+    LEDOUTPUT = (bool)docIpt[BusNumber];
+    Serial.println((bool)docIpt[BusNumber].as<int>());
+  }
+}
 
 void handleUserRequest(){
   String reqUri = httpServer.uri();
@@ -79,6 +190,13 @@ void handleUserRequest(){
   }
 }
 
+void handleRoot(){
+  BusNumber = httpServer.arg("busLine");
+  Serial.print(BusNumber);
+  httpServer.sendHeader("Location", "/index.html");       
+  httpServer.send(303);
+}
+
 String getContentType(String filename){
   if(filename.endsWith(".htm")) return "text/html";
   else if(filename.endsWith(".html")) return "text/html";
@@ -89,6 +207,7 @@ String getContentType(String filename){
   else if(filename.endsWith(".jpg")) return "image/jpeg";
   else if(filename.endsWith(".ico")) return "image/x-icon";
   else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".json")) return "text/json";
   else if(filename.endsWith(".pdf")) return "application/x-pdf";
   else if(filename.endsWith(".zip")) return "application/x-zip";
   else if(filename.endsWith(".gz")) return "application/x-gzip";
